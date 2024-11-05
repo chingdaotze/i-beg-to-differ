13a3febee1534f0ac5e62d6e13ba0ab6e84f2d25
Michael Ching <chingdaotze@yahoo.com>
2024-11-04 23:05:15 -0800

    Fixes to DictTableWidget.

--- src/i_beg_to_differ/ui/view/main_window/main_widget/object_viewer/input_field_group_boxes/dict_table_widget/dict_table_widget.py
+++ src/i_beg_to_differ/ui/view/main_window/main_widget/object_viewer/input_field_group_boxes/dict_table_widget/dict_table_widget.py
@@ -4,21 +4,27 @@ from typing import (
 )
 
 from PySide6.QtWidgets import (
-    QTableWidget,
     QWidget,
+    QGridLayout,
+    QTableWidget,
+    QPushButton,
     QTableWidgetItem,
 )
-
-# FIXME: This class still isn't behaving right
+from PySide6.QtGui import Qt
 
 
 class DictTableWidget(
-    QTableWidget,
+    QWidget,
 ):
 
     values: Dict[Any, Any]
     key_column: str
     value_column: str
+
+    layout: QGridLayout
+    table: QTableWidget
+    add_button: QPushButton
+    delete_button: QPushButton
 
     def __init__(
         self,
@@ -28,33 +34,40 @@ class DictTableWidget(
         parent: QWidget | None = None,
     ):
 
-        QTableWidget.__init__(
+        QWidget.__init__(
             self,
             parent,
         )
 
         self.values = values
-
-        self.setColumnCount(
-            2,
-        )
-
-        self.setRowCount(
-            len(self.values) + 1,
-        )
-
         self.key_column = key_column
         self.value_column = value_column
 
-        self.setHorizontalHeaderLabels(
+        self.layout = QGridLayout()
+        self.setLayout(
+            self.layout,
+        )
+
+        # Construct table
+        self.table = QTableWidget(
+            0,
+            2,
+            self,
+        )
+
+        self.table.setHorizontalHeaderLabels(
             [
                 self.key_column,
                 self.value_column,
             ]
         )
 
-        self.verticalHeader().setVisible(
+        self.table.verticalHeader().setVisible(
             False,
+        )
+
+        self.table.setShowGrid(
+            True,
         )
 
         for key, value in self.values.items():
@@ -63,8 +76,57 @@ class DictTableWidget(
                 value=value,
             )
 
-        self.cellChanged.connect(
+        self.table.cellChanged.connect(
             self.cell_changed,
+        )
+
+        self.layout.addWidget(
+            self.table,
+            0,
+            0,
+        )
+
+        # Construct buttons
+        button_layout = QGridLayout()
+
+        self.add_button = QPushButton(
+            'Add',
+            self,
+        )
+
+        self.delete_button = QPushButton(
+            'Delete',
+            self,
+        )
+
+        self.add_button.clicked.connect(
+            self.add_row,
+        )
+
+        self.delete_button.clicked.connect(
+            self.delete_current_row,
+        )
+
+        button_layout.addWidget(
+            self.add_button,
+            0,
+            0,
+        )
+
+        button_layout.addWidget(
+            self.delete_button,
+            1,
+            0,
+        )
+
+        button_layout.setAlignment(
+            Qt.AlignmentFlag.AlignTop,
+        )
+
+        self.layout.addLayout(
+            button_layout,
+            0,
+            1,
         )
 
     def append_row(
@@ -73,7 +135,9 @@ class DictTableWidget(
         value: Any,
     ) -> None:
         # Calculate new row
-        row = self.rowCount() + 1
+        self.add_row()
+
+        row = self.table.rowCount()
 
         key_widget = QTableWidgetItem(
             key,
@@ -83,14 +147,14 @@ class DictTableWidget(
             value,
         )
 
-        self.setItem(
-            row,
+        self.table.setItem(
+            row - 1,
             0,
             key_widget,
         )
 
-        self.setItem(
-            row,
+        self.table.setItem(
+            row - 1,
             1,
             value_widget,
         )
@@ -101,18 +165,47 @@ class DictTableWidget(
         column: int,
     ) -> None:
 
-        self.values.clear()
+        key = self.table.item(
+            row,
+            0,
+        )
 
-        for row in range(self.rowCount()):
+        if isinstance(key, QTableWidgetItem):
+            key = key.text()
 
-            key = self.itemAt(
-                row,
-                0,
-            ).text()
+        value = self.table.item(
+            row,
+            1,
+        )
 
-            value = self.itemAt(
-                row,
-                1,
-            ).text()
+        if isinstance(value, QTableWidgetItem):
+            value = value.text()
 
-            self.values[key] = value
+        self.values[key] = value
+
+    def add_row(
+        self,
+    ) -> None:
+
+        self.table.insertRow(
+            self.table.rowCount(),
+        )
+
+    def delete_current_row(
+        self,
+    ) -> None:
+
+        row = self.table.currentRow()
+
+        key = self.table.item(
+            row,
+            0,
+        )
+
+        if isinstance(key, QTableWidgetItem):
+            key = key.text()
+            del self.values[key]
+
+        self.table.removeRow(
+            row,
+        )
